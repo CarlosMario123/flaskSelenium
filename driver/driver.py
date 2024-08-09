a40801d6a9083f4967072ccde65bac094d34078c
CarlosMario123 <221220@ids.upchiapas.edu.mx>
2024-08-08 23:45:14 -0600

    driverAuto branch created

--- driver/driver.py
+++ driver/driver.py
@@ -1,30 +1,39 @@
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import StaleElementReferenceException
-
 from time import sleep
-
+from webdriver_manager.firefox import GeckoDriverManager
 
 class DriverGeko:
     def __init__(self, url):
+        # Configura las opciones del navegador
         self.options = Options()
-        self.options.headless = True
-        self.driver = webdriver.Firefox(options=self.options)
+        self.options.headless = True  # Ejecuta el navegador en modo headless (sin interfaz gráfica)
+        
+        # Inicializa el navegador con el controlador adecuado
+        self.driver = webdriver.Firefox(service=webdriver.firefox.service.Service(GeckoDriverManager().install()), options=self.options)
         self.url = url  # URL a scrapear
 
     def scrapearImg(self):
         self.driver.get(self.url)
         try:
-            # Esperar hasta que todas las imágenes estén presentes en la página
+            # Espera hasta que al menos una imagen esté presente en la página
             WebDriverWait(self.driver, 10).until(EC.presence_of_all_elements_located((By.TAG_NAME, 'img')))
         
+            # Encuentra todas las imágenes en la página
             imagenes = self.driver.find_elements(By.TAG_NAME, 'img')
             return imagenes
         except StaleElementReferenceException:
-            # Manejar la excepción y volver a intentar la operación después de un breve retraso
+            # Maneja la excepción y vuelve a intentar después de un breve retraso
             sleep(1)
-            return self.scrapearImg()  # Volver a intentar la operación
+            return self.scrapearImg()  # Reintenta la operación
+        except NoSuchElementException:
+            # Maneja el caso cuando no se encuentran elementos
+            print("No se encontraron imágenes en la página.")
+            return []
+        finally:
+            # Asegúrate de cerrar el navegador después de usarlo
+            self.driver.quit()
